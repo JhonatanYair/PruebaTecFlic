30c4f8df433edeb0643c0f80a40d7dbb205cd052
Jhonatan Peinado <jonathanpeinado8@gmail.com>
2024-06-19 20:12:21 -0500

    proyecto prueba Flic

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# PruebaTecFlic
+# Prueba Técnica con Angular, SQL y .NET
+
+Este repositorio contiene una prueba técnica que utiliza Angular para el frontend, SQL Server para la base de datos y .NET para el backend. Para desplegar y probar la aplicación, sigue los pasos detallados a continuación.
+
+## Despliegue de la Aplicación
+
+1. **Requisitos Previos:**
+   Asegúrate de tener Docker instalado en tu máquina. Puedes descargarlo desde [Docker Hub](https://www.docker.com/get-started).
+
+2. **Ejecución de la Aplicación:**
+   En la raíz del proyecto, ejecuta el siguiente comando para iniciar los contenedores Docker en segundo plano:
+
+   ```bash
+   docker-compose up -d
+   ```
+
+   Esto iniciará los contenedores de Angular, SQL Server y .NET necesarios para la aplicación.
+
+4. **Acceder a la Aplicación:**
+   Una vez que los contenedores estén en ejecución, puedes acceder a la aplicación Angular desde `http://localhost:4300`.
+
+## Endpoints de la API (CRUD de Productos)
+
+La API proporciona endpoints para realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) sobre los productos. A continuación se detallan los endpoints disponibles junto con ejemplos de cómo realizar las peticiones utilizando Postman:
+
+### Obtener Todos los Productos
+
+- **Método:** GET
+- **URL:** `http://localhost:8083/api/Producto/ListarProductos`
+- **Descripción:** Obtiene todos los productos almacenados en la base de datos.
+
+### Obtener un Producto por ID
+
+- **Método:** GET
+- **URL:** `http://localhost:8083/api/Producto/ListarProductos/{id}`
+- **Descripción:** Obtiene un producto específico mediante su ID.
+
+### Crear un Nuevo Producto
+
+- **Método:** POST
+- **URL:** `http://localhost:8083/api/Producto/CrearProducto`
+- **Descripción:** Crea un nuevo producto en la base de datos.
+
+  **Body:**
+  ```json
+    {
+        "nombre": "Nuevo Producto",
+        "precio": 29.99,
+        "cantidad": 23330,
+        "categoriaId": {categoriaId:int}
+    }
+  ```
+
+### Actualizar un Producto Existente
+
+- **Método:** PUT
+- **URL:** `http://localhost:8083/api/Producto/EditarProducto/{id}`
+- **Descripción:** Actualiza un producto existente en la base de datos.
+
+  **Body:**
+  ```json
+    {
+        "nombre": "Editar Producto",
+        "precio": 29.99,
+        "cantidad": 23330,
+        "categoriaId": {categoriaId:int}
+    }
+  ```
+
+### Eliminar un Producto
+
+- **Método:** DELETE
+- **URL:** `http://localhost:8083/api/Producto/EliminarProducto/{id}`
+- **Descripción:** Elimina un producto existente de la base de datos.
+
+### Obtener Todos las Categorías
+
+- **Método:** GET
+- **URL:** `http://localhost:8083/api/Categoria/ListarCategorias`
+- **Descripción:** Obtiene todas las categorías almacenados en la base de datos.
+
+---
+
+Este `README.md` proporciona una guía para desplegar la aplicación y utilizar los endpoints de la API CRUD para productos, usando ejemplos específicos de peticiones HTTP que se pueden realizar con Postman. Asegúrate de ajustar los detalles según las necesidades específicas y configuraciones de tu proyecto.
--- docker/sqlserver/DBScript.sql
+++ docker/sqlserver/DBScript.sql
@@ -1,15 +1,15 @@
 USE [master]
 GO
-/****** Object:  Database [AseoSas]    Script Date: 19/06/2024 1:56:04 p. m. ******/
+/****** Object:  Database [AseoSas]    Script Date: 19/06/2024 8:02:58 p. m. ******/
 CREATE DATABASE [AseoSas]
  CONTAINMENT = NONE
  ON  PRIMARY 
-( NAME = N'AseoSas', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\AseoSas.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+( NAME = N'AseoSas', FILENAME = N'/var/opt/mssql/data/AseoSas.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
  LOG ON 
-( NAME = N'AseoSas_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\AseoSas_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+( NAME = N'AseoSas_log', FILENAME = N'/var/opt/mssql/data/AseoSas_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
-ALTER DATABASE [AseoSas] SET COMPATIBILITY_LEVEL = 160
+ALTER DATABASE [AseoSas] SET COMPATIBILITY_LEVEL = 150
 GO
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
@@ -84,32 +84,32 @@ ALTER DATABASE [AseoSas] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_P
 GO
 USE [AseoSas]
 GO
-/****** Object:  Table [dbo].[Categorias]    Script Date: 19/06/2024 1:56:05 p. m. ******/
+/****** Object:  Table [dbo].[Categorias]    Script Date: 19/06/2024 8:02:59 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Categorias](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Nombre] [varchar](30) NULL,
-PRIMARY KEY CLUSTERED 
+	[Nombre] [varchar](100) NULL,
+ CONSTRAINT [PK__Categori__3214EC0782EEE7A8] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Productos]    Script Date: 19/06/2024 1:56:05 p. m. ******/
+/****** Object:  Table [dbo].[Productos]    Script Date: 19/06/2024 8:02:59 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Productos](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Nombre] [varchar](80) NULL,
+	[Nombre] [varchar](100) NULL,
 	[Precio] [float] NULL,
 	[Cantidad] [int] NULL,
 	[CategoriaId] [int] NULL,
-PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK__Producto__3214EC079053FE69] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
@@ -117,61 +117,55 @@ PRIMARY KEY CLUSTERED
 GO
 SET IDENTITY_INSERT [dbo].[Categorias] ON 
 GO
-INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1003, N'Categoría 0')
+INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1003, N'Productos de Aseo Personal')
 GO
-INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1004, N'Categoría 1')
+INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1004, N'Productos de Limpieza del Hogar')
 GO
-INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1005, N'Categoría 2')
+INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1005, N'Productos de Cuidado del Cuerpo')
 GO
-INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1006, N'Categoría 3')
+INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1006, N'Productos de Higiene Infantil')
 GO
-INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1007, N'Categoría 4')
+INSERT [dbo].[Categorias] ([Id], [Nombre]) VALUES (1007, N'Categoría General')
 GO
 SET IDENTITY_INSERT [dbo].[Categorias] OFF
 GO
 SET IDENTITY_INSERT [dbo].[Productos] ON 
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1009, N'Handmade Wooden Shoes', 37.239952087402344, 459, 1006)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1009, N'Jabón de Manos Artesanal', 37.24, 459, 1006)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1010, N'Practical Frozen Pants', 82.076080322265625, 9309, 1003)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1012, N'Esponja de Baño de Goma', 7.82, 1568, 1004)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1011, N'Awesome Steel Bike', 88.929008483886719, 9191, 1003)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1013, N'Crema Corporal de Madera', 47.01, 4679, 1004)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1012, N'Awesome Rubber Soap', 7.8190960884094238, 1568, 1004)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1015, N'Champú de Algodón Refinado', 62.64, 4189, 1004)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1013, N'Rustic Wooden Cheese', 47.007350921630859, 4679, 1004)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1016, N'Jabón de Tocador de Granito', 8.5, 8461, 1004)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1014, N'Ergonomic Granite Gloves', 88.045379638671875, 6499, 1005)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1017, N'Toallas Suaves de Algodón', 99.23, 2273, 1003)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1015, N'Refined Cotton Cheese', 62.640625, 4189, 1004)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1018, N'Jabón Líquido de Manos', 36.37, 4541, 1006)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1016, N'Generic Granite Bacon', 8.4969205856323242, 8461, 1004)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1019, N'Pañales de Goma Rústicos', 72.87, 1120, 1003)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1017, N'Handcrafted Soft Towels', 99.226226806640625, 2273, 1003)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1021, N'Esponja Facial de Acero', 4.84, 13, 1003)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1018, N'Sleek Wooden Soap', 36.372959136962891, 4541, 1006)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1023, N'Zapatos de Baño de Granito', 58.86, 8148, 1006)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1019, N'Rustic Rubber Towels', 72.874160766601562, 1120, 1003)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1024, N'Guantes de Plástico para Limpieza', 91.86, 376, 1005)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1020, N'Practical Metal Chair', 25.56024169921875, 2736, 1003)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1025, N'Jabón de Manos de Lujo', 39.57, 9790, 1005)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1021, N'Ergonomic Steel Hat', 4.8411445617675781, 13, 1003)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1026, N'Gel de Baño de Manos', 72.36, 6704, 1004)
 GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1022, N'Awesome Cotton Computer', 4.7864837646484375, 4466, 1005)
-GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1023, N'Refined Granite Shoes', 58.859813690185547, 8148, 1006)
-GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1024, N'Handcrafted Plastic Gloves', 91.861381530761719, 376, 1005)
-GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1025, N'Incredible Fresh Tuna', 39.566085815429688, 9790, 1005)
-GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1026, N'Handmade Frozen Sausages', 72.361251831054688, 6704, 1004)
-GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1027, N'Practical Metal Tuna', 41.119026184082031, 7881, 1005)
-GO
-INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1028, N'Sleek Wooden Computer', 4.0815167427062988, 9261, 1004)
+INSERT [dbo].[Productos] ([Id], [Nombre], [Precio], [Cantidad], [CategoriaId]) VALUES (1028, N'Computadora de Limpieza de Madera', 4.08, 9261, 1004)
 GO
 SET IDENTITY_INSERT [dbo].[Productos] OFF
+GO
+/****** Object:  Index [IX_Productos_CategoriaId]    Script Date: 19/06/2024 8:02:59 p. m. ******/
+CREATE NONCLUSTERED INDEX [IX_Productos_CategoriaId] ON [dbo].[Productos]
+(
+	[CategoriaId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Productos]  WITH CHECK ADD  CONSTRAINT [FK_Categoria] FOREIGN KEY([CategoriaId])
 REFERENCES [dbo].[Categorias] ([Id])
